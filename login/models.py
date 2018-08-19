c6df41247573aff460ceffe9907e62c950f9b327
Vanessa <yuna.nyan@live.cl>
2018-08-19 03:21:28 -0300

    Registro de usuario correcto.

--- login/models.py
+++ login/models.py
@@ -3,6 +3,7 @@ from django.db import models
 class Usuario(models.Model):
     username = models.CharField(max_length=50,blank=False,null=True)
     password = models.CharField(max_length=30,blank=False,null=True)
+    email = models.CharField(max_length=30,blank=False,null=True)
     tipo_usuario = models.IntegerField(default=0,blank=False,null=False)
 
     def __str__(self):
--- login/views.py
+++ login/views.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView
+from login.forms import CreateUserForm
 
 class RegistroUsuario(CreateView):
     model = User
     template_name = "login/registrar.html"
-    form_class = UserCreationForm
-    success_url = 'Home'
+    form_class = CreateUserForm
+    success_message = "Usuario registrado"
+    success_url = "http://127.0.0.1:8000/"
 
 # Create your views here.
