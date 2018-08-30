d2d0c3bf08f3ef8526937fba80bb13dc7ca98eeb
Vanessa <yuna.nyan@live.cl>
2018-08-29 21:24:20 -0300

    Cambio menor

--- Orden/views.py
+++ Orden/views.py
@@ -9,6 +9,9 @@ import xmlrpc.client
 # Create your views here.
 
 def ordenes_from_erp(request):
+    if not request.user.is_authenticated:
+        return HttpResponseRedirect('/?next=%s' % request.path)
+        
     info = xmlrpc.client.ServerProxy('https://demo.odoo.com/start').start()
     url, db, username, password = info['host'], info['database'], info['user'], info['password']
     common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
